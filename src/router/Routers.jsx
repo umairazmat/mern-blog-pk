0bbd46bf4d700eb5c34c01a6d2911849c0718d92
Umair Azmat <devumair260@gmail.com>
2023-10-12 18:30:04 +0500

    updated

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
-import Blogs from "../pages/blogs/Blogs";
-import About from "../pages/about/About";
-import Users from "../pages/users/Users";
+import AddBlog from "../pages/blogs/AddBlog";
 import PageNotFound from "../pages/page-not-found/PageNotFound";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
@@ -17,9 +15,7 @@ export default function Routers() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="about" element={<About />} />
-          <Route path="users" element={<Users />} />
+          <Route path="add-blog" element={<AddBlog />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
