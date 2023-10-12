8d7617426d428abef385d189dac7274424e3af2a
Umair Azmat <devumair260@gmail.com>
2023-10-12 18:30:12 +0500

    updated

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BlogPostCard from "../../components/cards/BlogPostCard";
+import Pagination from "../../components/pagination/Pagination";
 
 export default function Blogs() {
   return (
     <>
       <hr />
-      <div className="md:container sm:mx-auto">
-        <h2>This is Blog Home Page</h2>
+      <div className="md:container sm:mx-auto items-center">
         <BlogPostCard />
+        <Pagination />
       </div>
     </>
   );
