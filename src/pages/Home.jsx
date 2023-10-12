8b240707e49440dbece36ed2755bc6a08f568cea
Umair Azmat <devumair260@gmail.com>
2023-10-12 13:42:19 +0500

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Blogs from "./Blogs";
+
+export default function Home() {
+  return (
+    <>
+      <h1>This is Home Page</h1>
+      <Blogs />
+    </>
+  );
+}
