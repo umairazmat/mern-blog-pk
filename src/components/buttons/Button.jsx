e191d55dd06bcaaf2fbe74fab23e69d3e5866440
Umair Azmat <devumair260@gmail.com>
2023-10-12 18:29:57 +0500

    btn added

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 
 export default function Button({ btnText }) {
   return (
-    <button
-      type="button"
-      className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-    >
-      {btnText}
-    </button>
+    <div className="mt-14 text-center">
+      <button
+        type="button"
+        className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        {btnText}
+      </button>
+    </div>
   );
 }
 
