c50f28352d0da9b7d3d6fca0daeffa3c7f321ddd
Umair Azmat <devumair260@gmail.com>
2023-10-17 09:03:46 -0700

    multiple tags added

--- src/data/utils.jsx
+++ src/data/utils.jsx
@@ -6,7 +6,7 @@ const blogPostJson = [
     heading: "About Macbook",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Macbook",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const blogPostJson = [
     heading: "About Iphone",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Iphone",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
   },
   {
     id: 3,
@@ -24,61 +24,7 @@ const blogPostJson = [
     heading: "About Apple Watch",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Watch",
-  },
-  {
-    id: 4,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Macbook",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Macbook",
-  },
-  {
-    id: 5,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Iphone",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Iphone",
-  },
-  {
-    id: 6,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Apple Watch",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Watch",
-  },
-  {
-    id: 7,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Macbook",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Macbook",
-  },
-  {
-    id: 8,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Iphone",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Iphone",
-  },
-  {
-    id: 9,
-    image:
-      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-    heading: "About Apple Watch",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
-    tag: "#Watch",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
   },
 ];
 
