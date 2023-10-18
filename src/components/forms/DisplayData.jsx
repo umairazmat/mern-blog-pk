56d9b11ea689f55e8f2344ec3bce3e52575be6bc
Umair Azmat <devumair260@gmail.com>
2023-10-18 06:56:36 -0700

    display data

---
+++ src/components/forms/DisplayData.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import blogData from "../../data/blogData.json";
+import Card from "../cards/Card";
+
+export default function DisplayData() {
+  // eslint-disable-next-line
+  console.log(blogData);
+  return (
+    <div>
+      {blogData.map((post) => (
+        <div className="" key={post.id}>
+          <Card
+            image={post.image}
+            heading={post.heading}
+            description={post.description}
+            tags={post.tags}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
