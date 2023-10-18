ea93272341dfadcd009ba5ba5ede830d596b7914
Umair Azmat <devumair260@gmail.com>
2023-10-18 06:55:20 -0700

    json data

--- src/data/utils.jsx
+++ src/data/utils.jsx
@@ -26,6 +26,33 @@ const blogPostJson = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
     tags: ["#Iphone", "#Macbook", "#Watch"],
   },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    heading: "About Macbook",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    heading: "About Iphone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    heading: "About Apple Watch",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?",
+    tags: ["#Iphone", "#Macbook", "#Watch"],
+  },
 ];
 
 export default blogPostJson;
