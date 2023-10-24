3eb87b17fdff7bd7e19cc1df2594a432722bd670
Umair Azmat <devumair260@gmail.com>
2023-10-24 05:25:23 -0700

    blog details updated html tags converted to design

--- src/components/blog-details/BlogDetails.jsx
+++ src/components/blog-details/BlogDetails.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 // import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
+import parse from "html-react-parser";
 
 export default function BlogDetails() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-    // Retrieve the blog post data based on the postId from localStorage
-    const storedBlogs = localStorage.getItem("blogData");
-    if (storedBlogs) {
-      const parsedBlogs = JSON.parse(storedBlogs);
-      const foundPost = parsedBlogs.find(
-        (blog) => blog.id === parseInt(id, 10),
-      );
+    // Ensure id is defined before attempting to retrieve data
+    if (id) {
+      const storedBlogs = localStorage.getItem("blogData");
+      if (storedBlogs) {
+        const parsedBlogs = JSON.parse(storedBlogs);
+        const foundPost = parsedBlogs.find(
+          (blog) => blog.id === id.split(":")[0],
+        );
 
-      if (foundPost) {
-        setPost(foundPost);
+        if (foundPost) {
+          setPost(foundPost);
+        }
       }
     }
   }, [id]);
+
   // eslint-disable-next-line
   console.log(post);
+
+  if (!post) {
+    return <div>Loading...</div>; // Optionally, you can add a loading state.
+  }
+
   return (
     <main className="mt-10">
       <div
@@ -46,25 +55,22 @@ export default function BlogDetails() {
           <h2 className="text-4xl font-semibold text-white leading-tight">
             {post?.title}
           </h2>
-          <div className="mt-4">
-            {post?.tags &&
-              post.tags.map((tag) => (
-                <div
-                  key={tag} // Use the tag string as the key
-                  className="px-4 py-1 mx-2 mb-2 bg-black text-gray-200 inline-flex items-center justify-center"
-                >
-                  {tag}
-                </div>
-              ))}
-          </div>
+          <div className="mt-4 text-white">User Details</div>
         </div>
       </div>
-
       <div className="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
-        <p className="pb-6">{post?.description}</p>
+        <p className="pb-6">{parse(post?.description)}</p>
 
         <div className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
-          {post?.tags}
+          {post?.tags &&
+            post.tags.map((tag) => (
+              <div
+                key={tag} // Use the tag string as the key
+                className="px-4 py-1 mx-2 mb-2 bg-black text-white inline-flex items-center justify-center"
+              >
+                {tag}
+              </div>
+            ))}
         </div>
       </div>
     </main>
